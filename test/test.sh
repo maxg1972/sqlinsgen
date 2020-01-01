3acd35d099e71415e4970e6a18d93c1b765f637c
maxg1972 <maxg1972@gmail.com>
2020-01-01 11:35:09 +0100

    Fix test script runtime error

--- test/test.sh
+++ test/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python ../sqlinsgen.py --table-name=STAG_Outbound --source-file=dbeaver.csv --output-file=dbeaver.sql --profile=classic_csv
-python ../sqlinsgen.py --table-name=STAG_Outbound --source-file=ssms.csv --output-file=ssms.sql --profile=tabbed_csv
+python3 ../sqlinsgen.py --table-name=STAG_Outbound --source-file=dbeaver.csv --output-file=dbeaver.sql --profile=classic_csv
+python3 ../sqlinsgen.py --table-name=STAG_Outbound --source-file=ssms.csv --output-file=ssms.sql --profile=tabbed_csv
